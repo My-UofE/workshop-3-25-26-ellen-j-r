7de12bc5632c90b5a62335641b0960187f412627
ellen-j-r <ejr233@exeter.ac.uk>
2026-01-30 13:12:52 +0000

    NumbersInSpanish completed with switch-case

--- NumbersInSpanish.java
+++ NumbersInSpanish.java
@@ -4,18 +4,35 @@ public class NumbersInSpanish {
        int number = Integer.parseInt(args[0]); 
  
        // delete the line below and replace with your code
-       if (number == 1) {
-         System.out.println("uno");
-        } else if (number == 2 ) {
-         System.out.println("dos");
-        } else if (number == 3 ) {
-            System.out.println("tres");
-        } else if (number == 4){
-            System.out.println("cuatro");
-        }else if (number == 5){
-            System.out.println("cinco");
-        }else{
-            System.out.println("Sorry I do not know that!");
+    //    if (number == 1) {
+    //      System.out.println("uno");
+    //     } else if (number == 2 ) {
+    //      System.out.println("dos");
+    //     } else if (number == 3 ) {
+    //         System.out.println("tres");
+    //     } else if (number == 4){
+    //         System.out.println("cuatro");
+    //     }else if (number == 5){
+    //         System.out.println("cinco");
+    //     }else{
+    //         System.out.println("Sorry I do not know that!");
+    //     }
+
+
+        // Using switch
+        switch(number) {
+        case 1:
+           System.out.println( "uno" ); break; // don't forget the break after each case!
+        case 2:
+           System.out.println( "dos" ); break;
+        case 3:
+           System.out.println( "tres" ); break;
+        case 4:
+           System.out.println( "cuatro" ); break;
+        case 5:
+           System.out.println( "cinco" ); break;   
+    
+        default: System.out.println( "Sorry I do not know that!" );
         }
     }
  }
